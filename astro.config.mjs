5809ac6c242ddb3c7e866bef7e1a968fed6170e7
valeipr <valeria.peredorodriguez@gmail.com>
2025-03-13 18:28:01 -0400

    config base 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://valeipr.github.io',
-  base: 'etnografia-pueblo-chico/',
+  // site: 'https://valeipr.github.io',
+  // base: 'etnografia-pueblo-chico/',
   vite: {
     plugins: [tailwindcss()]
   }
